c8ad754c93c9ec1cfdac21f4a7cea358db6a0028
Nach098 <144853837+Nach098@users.noreply.github.com>
2024-11-10 22:12:33 -0300

    Add files via upload

--- helpers/Mysqldatabase.php
+++ helpers/Mysqldatabase.php
@@ -3,24 +3,34 @@
 class MySqlDatabase
 {
     private $connection;
+
     public function __construct($host, $username, $password, $database)
     {
-        $this -> connection = mysqli_connect($host, $username, $password, $database);
-        if(!$this -> connection){
+        $this->connection = mysqli_connect($host, $username, $password, $database);
+        if (!$this->connection) {
             die("Connection failed: " . mysqli_connect_error());
         }
     }
 
-    public function query($sql){
-        $result = mysqli_query($this -> connection, $sql);
+    public function query($sql)
+    {
+        $result = mysqli_query($this->connection, $sql);
         return mysqli_fetch_all($result, MYSQLI_ASSOC);
     }
 
-    public function execute($sql) {
+    public function prepare($sql)
+    {
+        // Prepara una consulta SQL y devuelve un objeto de sentencia preparada
+        return $this->connection->prepare($sql);
+    }
+
+    public function execute($sql)
+    {
         mysqli_query($this->connection, $sql);
     }
 
-    public function __destruct(){
-        mysqli_close($this -> connection);
+    public function __destruct()
+    {
+        mysqli_close($this->connection);
     }
 }
